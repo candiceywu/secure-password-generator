fbdcdbd075afefe9ea2a72924f6a92bf8382ffd6
candiceywu <candice.ying.wu@gmail.com>
2021-08-14 15:48:49 -0700

    final js code but need to update readme

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ function writePassword() {
     passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // array of potential characters
 var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericalCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "`", "-", "+", "=", ",", ".", "<", ">", "?", "/"];
+var selectedArray = [];
 var finalArray = [];
 
 // Ask user to generate password by clicking on Generate Password button
@@ -25,8 +23,6 @@ function generatePassword() {
         passwordLength = window.prompt("Please enter a password length between 8 and 128 characters:");
     };
     console.log(passwordLength);
-
-
 
     //Ask the user to confirm choice of lowercase: 
     var userLowercase = window.confirm("Do you want to include lowercase characters?");
@@ -43,7 +39,6 @@ function generatePassword() {
     //Ask the user to confirm choice of special characters: 
     var userSpecial = window.confirm("Do you want to include special characters?");
     console.log(userSpecial);
-
 
     // If the user doesn't select at least one option, it will cycle through this again
     while (!userLowercase && !userUppercase && !userNumerical && !userSpecial) {
@@ -67,33 +62,49 @@ function generatePassword() {
     //takes the user's selections and adds them into the array of potential characters
 
     if (userLowercase) {
-        finalArray = finalArray.concat(lowercaseCharacters);
+        selectedArray = selectedArray.concat(lowercaseCharacters);
+        console.log(selectedArray);
     } if (userUppercase) {
-        finalArray = finalArray.concat(uppercaseCharacters);
+        selectedArray = selectedArray.concat(uppercaseCharacters);
+        console.log(selectedArray);
     } if (userNumerical) {
-        finalArray = finalArray.concat(numericalCharacters);
+        selectedArray = selectedArray.concat(numericalCharacters);
+        console.log(selectedArray);
     } if (userSpecial) {
-        finalArray = finalArray.concat(specialCharacters);
+        selectedArray = selectedArray.concat(specialCharacters);
+        console.log(selectedArray);
     }
 
     // need to guarantee that all user selection(s) will be included in password
-    // // 0 = lowercaseCharacters, 1 = uppercaseCharacters, 2 = numericalCharacters, 3 = specialCharacters
-    // for (var i = 0; i < length; i++) {
-    //     var randomOption = Math.floor(Math.random() * 4);
-    //     if (randomOption == 0 && !userLowercase)
-    //     //choose uppercaseCharacters
-    //     finalArray.push(uppercaseCharacters[Math.floor(Math.random() * 3)]);
+    if (userLowercase) {
+        finalArray = finalArray.concat(lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)]);
+        console.log(finalArray);
+    } if (userUppercase) {
+        finalArray = finalArray.concat(uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)]);
+        console.log(finalArray);
+    } if (userNumerical) {
+        finalArray = finalArray.concat(numericalCharacters[Math.floor(Math.random() * numericalCharacters.length)]);
+        console.log(finalArray);
+    } if (userSpecial) {
+        finalArray = finalArray.concat(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+        console.log(finalArray);
+    }
 
-    // }
+    // put together all selected choices into the length of password
 
     var string = "";
-    for (var i = 0; i < length; i++) {
-        string = string + finalArray[Math.floor(Math.random() * finalArray.length)]; 
+    for (var i = finalArray.length; i < passwordLength; i++) {
+        finalArray = finalArray.concat(selectedArray[Math.floor(Math.random() * selectedArray.length)]);
+        console.log(string);
     }
-    console.log(string); //need to fix something in this 
 
-    console.log(finalArray);
-    return string;
+    // convert final array into string, remove commas
+    finalArray = finalArray.join("");
+
+    return finalArray;
+    
 }
 
-//then you have to display the password in the "#password" placeholder text - either textContent or append?
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
